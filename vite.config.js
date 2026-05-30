83fe4a6d612e3c2f428469a669ac1a66bf4f662c
Harshvardhan Dhankhar <harshvardhan@Harshvardhans-MacBook-Pro.local>
2026-05-30 16:26:41 +0530

    Live Host-2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/harshvardhan0809/PitchIQ.git',
   server: {
     proxy: {
       '/api': 'http://127.0.0.1:3001',
